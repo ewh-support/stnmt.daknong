34175a7a1b8aed9dbf41b64088178c99f980de3c
daknong <daknong@email.com>
2019-04-11 10:55:35 +0700

    Thu 11/04/2019 10:55:30.15

---
+++ _layouts/blank.js
@@ -0,0 +1,23 @@
+function ULSaew(){var o=new Object;o.ULSTeamName="Microsoft SharePoint Foundation";o.ULSFileName="blank.js";return o;}
+
+/*
+     FILE ARCHIVED ON 06:27:00 Jun 01, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:53:05 Apr 09, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 142.129 (3)
+  esindex: 0.006
+  captures_list: 158.735
+  CDXLines.iter: 9.832 (3)
+  PetaboxLoader3.datanode: 92.822 (5)
+  exclusion.robots: 0.159
+  exclusion.robots.policy: 0.151
+  RedisCDXSource: 4.557
+  PetaboxLoader3.resolve: 172.277 (3)
+  load_resource: 143.479
+*/
